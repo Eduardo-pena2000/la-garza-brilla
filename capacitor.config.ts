0076e7311b6feacb16dcd156b5a3cb6ccb8d5b97
Eduardo <eduardopenacontacto@gmail.com>
2026-09-14 01:58:53 -0600

    fix(android): create separate static build for Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.lagarza.app',
   appName: 'Lotería La Garza',
   webDir: 'dist',
-  server: {
-    url: 'http://localhost:5173',
-    cleartext: true
-  }
 };
 
 export default config;
